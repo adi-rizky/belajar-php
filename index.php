bf335ddce0373a387e7d41d689a58ceefb19eec2
Adi Rizky Setiawan <adirizky472@gmail.com>
2022-11-11 15:05:16 +0700

    tugas 8

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama ="Adi Rizky Setiawan";
+$umur =20;
+
+echo "Saya <strong>$nama, saya berusia $umur tahun.</br>";
+
+$namaAdik ="guntur";
+$umurAdik ="15";
+
+echo "Adik saya bernama $namaAdik, adik saya berusia
+$umurAdik tahun.<br>";
+
+$selisihumur = $umur - $umurAdik;
+echo "Selisih kami adalah $selisihumur tahun.";
+
+?>
